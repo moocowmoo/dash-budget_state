8cbc95ecf3c6506f4a4a9c2bc941ad71b8875a03
moocowmoo <moocowmoo@masternode.me>
2016-01-28 07:42:18 +0000

    show next 3 budgets - cleanup

--- README.md
+++ README.md
@@ -3,16 +3,34 @@ simple listing of current dash budget disbursement
 
 # sample output
 
-    current time      : Thu, 28 Jan 2016 06:17:53 +0000
-    current block     : 410608
-    next budget cycle : 20.53 days (11824 blocks)
+    current time      : Thu, 28 Jan 2016 07:57:12 +0000
+    current block     : 410649
     
+    next budget :  8.25 days - block 415400 ( 4751 blocks)
     name                 approval payment        remaining
     estimated budget                         7714.57142857 
-    2mb-blocksize        99.8%       5.00    7709.57142857 
-    core-team            99.4%    1176.00    6533.57142857 
-    dash-org             98.5%    2100.00    4433.57142857 
-    lamassu-integration  97.2%     610.26    3823.31142857 
-    public-awareness     92.0%    2156.00    1667.31142857 
-    ds-liquidity         88.8%     140.00    1527.31142857 
-    transform-pr         68.3%    1100.00     427.31142857 
+    2mb-blocksize        99.8%       5.00    7709.57142857
+    core-team            99.4%    1176.00    6533.57142857
+    dash-org             98.5%    2100.00    4433.57142857
+    lamassu-integration  97.2%     610.26    3823.31142857
+    public-awareness     92.0%    2156.00    1667.31142857
+    ds-liquidity         88.8%     140.00    1527.31142857
+    transform-pr         68.3%    1100.00     427.31142857
+    
+    next budget : 37.10 days - block 448632 (21367 blocks)
+    name                 approval payment        remaining
+    estimated budget                         7163.53061224 
+    core-team            99.4%    1176.00    5987.53061224
+    dash-org             98.5%    2100.00    3887.53061224
+    lamassu-integration  97.2%     610.26    3277.27061224
+    public-awareness     92.0%    2156.00    1121.27061224
+    ds-liquidity         88.8%     140.00     981.27061224
+    transform-pr         68.3%    1100.00    -118.72938776
+    
+    next budget : 65.94 days - block 481864 (37983 blocks)
+    name                 approval payment        remaining
+    estimated budget                         7163.53061224 
+    core-team            99.4%    1176.00    5987.53061224
+    lamassu-integration  97.2%     610.26    5377.27061224
+    public-awareness     92.0%    2156.00    3221.27061224
+    ds-liquidity         88.8%     140.00    3081.27061224
--- budget_state.py
+++ budget_state.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+import time
 import yaml
 
 def run_command(cmd):
     return subprocess.check_output(cmd, shell=True).rstrip("\n")
+
+proposals = yaml.load(run_command("dash-cli mnbudget show"))
+pay_order = sorted(proposals.keys(),
+                   key=lambda n: proposals[n]['Ratio'])[::-1]
+current_block = int(run_command("dash-cli getblockcount"))
+total_masternodes = int(run_command("dash-cli masternode count"))
+cycle_length = 16616
 
 def min_blok_subsidy(nHeight):
     nSubsidy = float(5)  # assume minimum subsidy
@@ -12,45 +20,35 @@ def min_blok_subsidy(nHeight):
         nSubsidy -= nSubsidy/14
     return nSubsidy
 
-proposals = yaml.load(run_command("dash-cli mnbudget show"))
-pay_order = sorted(proposals.keys(),
-                   key=lambda n: proposals[n]['Ratio'])[::-1]
-now_block = int(run_command("dash-cli getblockcount"))
-total_masternodes = int(run_command("dash-cli masternode count"))
+def print_budget(proposals, current_block, cycle_offset):
+    current_block = current_block + (cycle_offset * cycle_length)
+    budget = cycle_length * .1 * min_blok_subsidy(current_block)
+    next_cycle = cycle_length - (current_block % cycle_length) + (cycle_offset * cycle_length)
+    next_cycle_block = current_block + next_cycle
+    print "next budget : {0:>5.2f} days - block {1:} ({2:>5} blocks)".format(
+            ((next_cycle * 2.5)/1440), next_cycle_block, next_cycle)
+    print "{0:<20} {1:<4} {2:<11} {3:>12}".format('name', 'approval', 'payment', 'remaining')
+    print "{0:<20}                {1:18.8f} ".format('estimated budget', budget)
+    for pname in pay_order:
+        p = proposals[pname]
+        if ( p["RemainingPaymentCount"] - cycle_offset) < 1:
+            # print " proposal %s payments fulfilled" % pname
+            continue
+        if p["BlockStart"] > next_cycle_block:
+            # print " proposal %s pending" % pname
+            continue
+        if p["BlockEnd"] < current_block:
+            # print " proposal %s expired" % pname
+            continue
+        if p["Yeas"] - p["Nays"] < int(total_masternodes/10):
+            # print " proposal %s rejected" % pname
+            continue
+        budget = budget - p['MonthlyPayment']
+        print "{0:<20} {1:2.1f}%  {2:9.2f} {3:16.8f}".format(pname, 100 * p['Ratio'], p['MonthlyPayment'], budget)
 
-cycle_length = 16616
-next_cycle = now_block % cycle_length
-next_cycle_block = now_block + next_cycle
-budget = 16616 * .1 * min_blok_subsidy(now_block)
-def days(s):
-    return 1
-
-import time
-print
-print "current time      : %s" % time.strftime("%a, %d %b %Y %H:%M:%S %z")
-
-print "current block     : %s" % now_block
-print "next budget cycle : %.2f days (%s blocks)" % (
-        ((next_cycle * 2.5)/1440), next_cycle)
-print
-print "{0:<20} {1:<4} {2:<11} {3:>12}".format('name', 'approval', 'payment', 'remaining')
-print "{0:<20}                {1:18.8f} ".format('estimated budget', budget)
-for pname in pay_order:
-    p = proposals[pname]
-    if p["RemainingPaymentCount"] < 1:
-        # print " proposal %s payments fulfilled" % pname
-        continue
-    if p["BlockStart"] > next_cycle_block:
-        # print " proposal %s pending" % pname
-        continue
-    if p["BlockEnd"] < now_block:
-        # print " proposal %s expired" % pname
-        continue
-    if p["Yeas"] - p["Nays"] < int(total_masternodes/10):
-        # print " proposal %s rejected" % pname
-        continue
-
-    budget = budget - p['MonthlyPayment']
-    print "{0:<20} {1:2.1f}%  {2:9.2f} {3:16.8f} ".format(pname, 100 * p['Ratio'], p['MonthlyPayment'], budget)
-
-print
+if __name__ == "__main__":
+    print "\ncurrent time      : %s" % time.strftime("%a, %d %b %Y %H:%M:%S %z")
+    print "current block     : %s\n" % current_block
+    for r in range(0,3):
+        print_budget(proposals, current_block, r)
+        print
